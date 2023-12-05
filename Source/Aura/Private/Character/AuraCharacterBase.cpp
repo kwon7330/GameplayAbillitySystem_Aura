2d14ad294bb244f3ae102e43306428ae074447ab
KSE <fplls12@gmail.com>
2023-12-05 19:40:45 +0900

    Aura & Goblins Add Anime

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,8 @@ AAuraCharacterBase::AAuraCharacterBase()
  	
 	PrimaryActorTick.bCanEverTick = false;
 
+	// 무기를 생성해서  Mesh의  WeaponHandSocket을 만들어 붙일것이다.
+	// 그리고 무기의 충돌처리를 NoCollision으로 한다.
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
 	Weapon-> SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
 	Weapon-> SetCollisionEnabled(ECollisionEnabled::NoCollision);

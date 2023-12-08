75d927c3d9623cd7f9040dd9870e2494d5cfcfe0
KSE <fplls12@gmail.com>
2023-12-08 13:47:38 +0900

    Create AuraPlayerState, add BP_AuraP/S, Change GameModeBase->PlayerState

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -11,9 +11,11 @@ AAuraEnemy::AAuraEnemy()
 	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
 }
 
+// Highlight를 켜주는 함수. PostProcessing_Mat를 사용하여 아웃라인을 생성시킨다.
 void AAuraEnemy::HighlightActor()
 {
 	GetMesh()->SetRenderCustomDepth(true);
+	// 메크로를 사용하여 250의 숫자를 CUSTOM_DEPTH_RED로 치환하였다. 
 	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 	Weapon->SetRenderCustomDepth(true);
 	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyrigth Druid Mechanics
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
